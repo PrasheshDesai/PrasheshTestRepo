df5e4e982fe506e4812e66bbce7dbe43c13ea23e
unknown <prashesh.desai@ics-global.in>
2022-05-10 13:10:01 +0530

    Class Added

---
+++ Class/Assignment_9/Default.aspx.vb
@@ -0,0 +1,8 @@
+﻿Public Class _Default
+    Inherits Page
+
+    Protected Sub Page_Load(ByVal sender As Object, ByVal e As EventArgs) Handles Me.Load
+        Console.WriteLine("hello, world")
+        Console.ReadKey()
+    End Sub
+End Class
